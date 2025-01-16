823696a5a3b38dab5b598209f462c7f32223bfe4
GokulKumar2003 <97659433+GokulKumar2003@users.noreply.github.com>
2025-01-16 09:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 64.4 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -2,22 +2,40 @@ class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         
+        // int num1Size = nums1.size(), num2Size = nums2.size();
+
+        // map<int, int> m; //freq
+        // for(int i=0;i<nums1.size();i++){
+        //     m[nums1[i]] += num2Size%2;
+        // }
+        // for(int i=0;i<nums2.size();i++){
+        //     m[nums2[i]] += num1Size%2;
+        // }
+
+        // int ans = 0;
+        // for(auto it:m){
+        //     if(it.second%2){
+        //         ans ^= it.first;
+        //     }
+        // }
+        // return ans;
+
+        // optimized approach
         int num1Size = nums1.size(), num2Size = nums2.size();
-
-        map<int, int> m; //freq
-        for(int i=0;i<nums1.size();i++){
-            m[nums1[i]] += num2Size%2;
-        }
-        for(int i=0;i<nums2.size();i++){
-            m[nums2[i]] += num1Size%2;
-        }
-
-        int ans = 0;
-        for(auto it:m){
-            if(it.second%2){
-                ans ^= it.first;
+        int xor1 = 0, xor2 = 0;
+        if(num1Size % 2){
+            for(int it : nums2){
+                xor1 ^= it;
             }
         }
-        return ans;
+
+        if(num2Size % 2){
+            for(int it : nums1){
+                xor2 ^= it;
+            }
+        }
+
+        return xor1 ^ xor2;
+
     }
 };
