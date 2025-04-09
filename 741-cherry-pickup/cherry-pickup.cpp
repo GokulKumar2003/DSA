69f3da9cfc15a77ec2070624b3aa43a91a221f92
GokulKumar2003 <97659433+GokulKumar2003@users.noreply.github.com>
2025-04-09 20:19:58 +0530

    Time: 635 ms (14.69%) | Memory: 565.6 MB (8.53%) - LeetSync

---
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int find(vector<vector<int>>& grid, int n, int r1, int c1, int r2, int c2, vector<vector<vector<vector<int>>>>& dp){
+
+        if(r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1){
+            return INT_MIN;
+        }
+        if(dp[r1][c1][r2][c2] != -1){
+            return dp[r1][c1][r2][c2];
+        }
+
+        if(r1 == n-1 && c1 == n-1){
+            return grid[r1][c1];
+        }
+
+        
+        int currentPick = grid[r1][c1];
+        if(c1 != c2){
+            currentPick += grid[r2][c2];
+        }
+
+        int rightRight = 0, rightDown = 0, downRight = 0, downDown = 0;
+
+        rightRight = find(grid, n, r1, c1 + 1, r2 , c2 + 1, dp);
+        rightDown = find(grid, n, r1, c1 + 1, r2 + 1, c2, dp);
+        downRight = find(grid, n, r1 + 1, c1, r2, c2 + 1, dp);
+        downDown = find(grid, n ,r1 + 1, c1, r2 + 1, c2, dp);
+
+        currentPick += max(rightRight, max(rightDown, max(downRight, downDown)));
+        return dp[r1][c1][r2][c2] = currentPick;
+        
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(n, vector<vector<int>>(n, vector<int>(n, -1))));
+        return max(0, find(grid, n, 0, 0, 0, 0, dp));
+    }
+};
