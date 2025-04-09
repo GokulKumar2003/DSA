eee2293d49821c9667cd7e9ee051cc51c931094a
GokulKumar2003 <97659433+GokulKumar2003@users.noreply.github.com>
2025-04-09 20:22:22 +0530

    Time: 35 ms (73.84%) | Memory: 27.3 MB (60.47%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int find(vector<vector<int>>& grid, int n, int r1, int c1, int r2, int c2, vector<vector<vector<vector<int>>>>& dp){
+    int find(vector<vector<int>>& grid, int n, int r1, int c1, int c2, vector<vector<vector<int>>>& dp){
 
+        int r2 = r1 + c1 - c2;
         if(r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1){
             return INT_MIN;
         }
-        if(dp[r1][c1][r2][c2] != -1){
-            return dp[r1][c1][r2][c2];
+        if(dp[r1][c1][c2] != -1){
+            return dp[r1][c1][c2];
         }
 
         if(r1 == n-1 && c1 == n-1){
@@ -21,19 +22,19 @@ public:
 
         int rightRight = 0, rightDown = 0, downRight = 0, downDown = 0;
 
-        rightRight = find(grid, n, r1, c1 + 1, r2 , c2 + 1, dp);
-        rightDown = find(grid, n, r1, c1 + 1, r2 + 1, c2, dp);
-        downRight = find(grid, n, r1 + 1, c1, r2, c2 + 1, dp);
-        downDown = find(grid, n ,r1 + 1, c1, r2 + 1, c2, dp);
+        rightRight = find(grid, n, r1, c1 + 1, c2 + 1, dp);
+        rightDown = find(grid, n, r1, c1 + 1, c2, dp);
+        downRight = find(grid, n, r1 + 1, c1, c2 + 1, dp);
+        downDown = find(grid, n ,r1 + 1, c1, c2, dp);
 
         currentPick += max(rightRight, max(rightDown, max(downRight, downDown)));
-        return dp[r1][c1][r2][c2] = currentPick;
+        return dp[r1][c1][c2] = currentPick;
         
     }
     int cherryPickup(vector<vector<int>>& grid) {
         
         int n = grid.size();
-        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(n, vector<vector<int>>(n, vector<int>(n, -1))));
-        return max(0, find(grid, n, 0, 0, 0, 0, dp));
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(n, -1)));
+        return max(0, find(grid, n, 0, 0, 0, dp));
     }
 };
