e916538f8505eefdba4a3803fd4cf7accb85f3b7
GokulKumar2003 <97659433+GokulKumar2003@users.noreply.github.com>
2025-04-17 23:46:48 +0530

    Time: 82 ms (58.56%) | Memory: 172.9 MB (90.24%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,86 @@
+class Node{
+public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int key, int val, Node* prev, Node* next){
+        this->key = key;
+        this->val = val;
+        this->prev = prev;
+        this->next = next;
+    }
+};
+class LRUCache {
+public:
+     int size;
+    Node* head;
+    Node* tail;
+    unordered_map<int, Node*> m;
+    LRUCache(int capacity) {
+        size = capacity;
+        head = new Node(-1, -1, NULL, NULL);
+        tail = new Node(-1, -1, NULL, NULL);
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        int value = -1;
+        if(m.find(key) != m.end()){
+            Node *nodeToBeUpdated = m[key];
+            value = nodeToBeUpdated->val;
+            deleteNode(nodeToBeUpdated);
+            insertNode(nodeToBeUpdated);
+        }
+        return value;
+    }
+    
+    void put(int key, int value) {
+         if(m.find(key) == m.end()){
+            Node *newNode = new Node(key, value, NULL, NULL);
+            if(m.size() == size){
+                int key = deleteLRU();
+                m.erase(key);
+            }
+            insertNode(newNode);
+            m[key] = newNode;
+        }
+        else{
+            Node *nodeToBeUpdated = m[key];
+            nodeToBeUpdated->val = value;
+            deleteNode(nodeToBeUpdated);
+            insertNode(nodeToBeUpdated);
+        }
+    }
+
+    int deleteLRU(){
+        int key = head->next->key;
+        head->next = head->next->next;
+        head->next->prev = head;
+        return key;
+    }
+
+    void insertNode(Node* newNode){
+        
+        newNode->next = tail;
+        newNode->prev = tail->prev;
+        tail->prev->next = newNode;
+        tail->prev = newNode;
+
+    }
+
+    void deleteNode(Node* node){
+
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
