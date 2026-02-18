1fbfdbd14921084567f96e39f715a82d05ee0822
GokulKumar2003 <97659433+GokulKumar2003@users.noreply.github.com>
2026-02-18 20:14:15 +0530

    Time: 856 ms (5.04%) | Memory: 313.2 MB (5.85%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int firstUniqueFreq(vector<int>& nums) {
         map<int, int> numToFreq;
-        map<int, set<int>> freqToNum;
+        map<int, int> freqToNum;
 
         for(int i=0;i<nums.size();i++){
             int prevFreq = 0, currFreq;
             if(numToFreq.find(nums[i]) != numToFreq.end()){
                 prevFreq = numToFreq[nums[i]];
-                freqToNum[prevFreq].erase(nums[i]);
+                freqToNum[prevFreq]--;
             }
             currFreq = prevFreq + 1;
-            freqToNum[currFreq].insert(nums[i]);
+            freqToNum[currFreq]++;
             numToFreq[nums[i]]++;
         }
         for(int i=0;i<nums.size();i++){
-            if(freqToNum[numToFreq[nums[i]]].size() == 1){
+            if(freqToNum[numToFreq[nums[i]]] == 1){
                 return nums[i];
             }
         }
