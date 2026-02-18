1742a64b6cb97d93639cda89472a0171c8a7fda9
GokulKumar2003 <97659433+GokulKumar2003@users.noreply.github.com>
2026-02-18 20:38:32 +0530

    Time: 2289 ms (9.98%) | Memory: 558.3 MB (6.24%) - LeetSync

--- 4221-longest-almost-palindromic-substring/longest-almost-palindromic-substring.cpp
+++ 4221-longest-almost-palindromic-substring/longest-almost-palindromic-substring.cpp
@@ -17,11 +17,11 @@ public:
                 r--;
             }
             else{
-                if(isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1)){
-                    return true;
+                if(!isPalindrome(s, l+1, r) && !isPalindrome(s, l, r-1)){
+                    return false;
                 }
                 else{
-                    return false;
+                    return true;
                 }
             }
         }
